8a724a3a00055d2bfb7af4965ad934cdc64c04cc
mergimselmani <m.selmani@heijtingmachinery.nl>
2025-10-12 00:39:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+// next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  experimental: { appDir: true },
+  webpack: (config) => {
+    // Zorg dat imports als "@/lib/..." en "@/components/..." werken
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
-module.exports = nextConfig;
