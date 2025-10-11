d3f29c14bbf289d140059d7827b76cbc3bec06a3
mergimselmani <m.selmani@heijtingmachinery.nl>
2025-10-11 23:40:21 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { typedRoutes: true },
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true },    // temp: let build pass
-  eslint: { ignoreDuringBuilds: true },       // temp: let build pass
-};
-module.exports = nextConfig;
